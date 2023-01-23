95d44e913fb51f79da922570b4312c0d54b112da
laurasanti-vgw <laura.santi@vgw.co>
2023-01-23 20:45:18 +0800

    adapted models and serializers

--- webApi/wallet/models.py
+++ webApi/wallet/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Wallet(models.Model):
+    id = models.CharField(max_length=200, primary_key=True)
     transactionId = models.CharField(max_length=200)
     version = models.IntegerField(null=True)
     coins = models.IntegerField()
--- webApi/wallet/serializers.py
+++ webApi/wallet/serializers.py
@@ -3,5 +3,5 @@ from rest_framework import serializers
 class WalletSerializer(serializers.Serializer):
     id = serializers.ReadOnlyField()
     transactionId = serializers.CharField(max_length=200)
-    version = serializers.IntegerField(null=True)
+    version = serializers.IntegerField()
     coins = serializers.IntegerField()
