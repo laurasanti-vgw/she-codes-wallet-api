6187631fc99470842a38b035bc3bc5a473053e66
laurasanti-vgw <laura.santi@vgw.co>
2023-01-23 21:40:29 +0800

    set up debit

--- webApi/wallet/urls.py
+++ webApi/wallet/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('<str:pk>/', views.WalletBalance.as_view()),
     path('<str:pk>/credit', views.CreditWallet.as_view()),
+    path('<str:pk>/debit', views.DebitWallet.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- webApi/wallet/views.py
+++ webApi/wallet/views.py
@@ -15,6 +15,7 @@ class WalletBalance(APIView):
         except Wallet.DoesNotExist:
             raise Http404
 
+
     def get(self, request, pk):
         wallet = self.get_object(pk)
         serializer = WalletSerializer(wallet)
@@ -22,7 +23,7 @@ class WalletBalance(APIView):
 
 class CreditWallet(APIView):
 
-    def credit_wallet(wallet, request):
+    def credit_wallet(self, wallet, request):
         payload=request.data
         
         wallet.transactionId = payload.get('transactionId')
@@ -35,20 +36,61 @@ class CreditWallet(APIView):
         payload=request.data
         
         wallet = Wallet.objects.filter(pk=pk).first()
-        serializer = WalletSerializer(wallet)
 
-        if wallet is not None:
-            wallet = self.credit_wallet(wallet, request)
-                
-        else:
-            transactionId = payload.get('transactionId')
-            version = 1
-            coins = payload.get('coins')
+        if wallet is None:
+            wallet, created = Wallet.objects.update_or_create(
+            id=pk, defaults={'transactionId': payload.get('transactionId'), 'version': 1, 'coins': payload.get('coins') },
+            )
+ 
+        wallet = self.credit_wallet(wallet, request)
 
-
-        wallet, created = Wallet.objects.update_or_create(
-            id=pk, defaults={'transactionId': transactionId, 'version': version, 'coins': coins },
-        )
         serializer = WalletSerializer(wallet)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+
+class DebitWallet(APIView):
+
+    def get_object(self, pk):
+        try:
+            wallet = Wallet.objects.get(pk=pk)
+            return wallet
+        except Wallet.DoesNotExist:
+            raise Http404
+
+
+    def debit_wallet(self, wallet, request):
+        payload=request.data
+
+        if check_for_negative_balance(wallet, request):
+            return "Insufficient funds"
+        else:
+            wallet.transactionId = payload.get('transactionId')
+            wallet.version = wallet.version + 1
+            wallet.coins = wallet.coins - payload.get('coins')
+            wallet.save()
+        return wallet
+
+
+    def post(self, request, pk):
+
+        wallet = self.get_object(pk)
+        serializer = WalletSerializer(wallet)
+
+        if wallet and check_for_negative_balance(wallet, request):
+            return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+            
+        wallet = self.debit_wallet(wallet, request)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+
+
+def check_for_negative_balance(wallet, request):
+    payload=request.data
+
+    currentBalance = wallet.coins
+    debitRequest = payload.get('coins')
+
+    if currentBalance - debitRequest < 0:
+        return True
+    return False
