c101fc76c9320b1ec7ce51b630ae6b2ea7f879e9
laurasanti-vgw <laura.santi@vgw.co>
2023-01-23 20:46:59 +0800

    get wallet view

--- webApi/wallet/views.py
+++ webApi/wallet/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.http import Http404, HttpResponse
+from .models import Wallet
+from .serializers import WalletSerializer
 
-# Create your views here.
+
+class WalletBalance(APIView):
+
+    def get_object(self, pk):
+        try:
+            wallet = Wallet.objects.get(pk=pk)
+            return wallet
+        except Wallet.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        wallet = self.get_object(pk)
+        serializer = WalletSerializer(wallet)
+        return Response(serializer.data)
