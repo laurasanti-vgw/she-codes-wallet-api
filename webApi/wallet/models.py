afca086eec3897f7d71ffdbb17845c582eb43f01
laurasanti-vgw <laura.santi@vgw.co>
2023-01-31 15:07:03 +0800

    simplified id

--- webApi/wallet/models.py
+++ webApi/wallet/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class Wallet(models.Model):
-    id = models.IntegerField(max_length=200, primary_key=True)
+    id = models.IntegerField(primary_key=True)
     coins = models.IntegerField()
